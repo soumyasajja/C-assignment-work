8a8b0d756aa5ea77f844fedbba6fb4f59a7d4dc8
soumyasajja <sajjasoumya@gmail.com>
2019-02-09 19:58:23 +0530

    conversion binary to hex and vice versa

--- main1/main1/b2hh2b.c
+++ main1/main1/b2hh2b.c
@@ -1,32 +1,87 @@
 #include <stdio.h>
 int binToHex(long long);
-//long long hexToBin(int);
+void hexToBin(char*);
 int main()
 {
 	long long bin;
-	int hex;
+	char hex[15];
 	printf("enter the binary value\n");
 	scanf("%lld", &bin);
 	printf("enter the hexadecimal value\n");
-	scanf("%x", &hex);
+	scanf("%s", &hex);
 	printf("the value of %lld in hexadecimal is %x\n", bin, binToHex(bin));
-	//printf("the value of %x in hexadecimal is %lld\n", hex, hexToBin(hex));
+    hexToBin(hex);
 }
 int binToHex(long long binary)
 {
-	int rem, i = 0, count = 0,res=0,dec=0;
+	int  i = 1, rem, hexa = 0;
 	while (binary != 0)
 	{
-		rem = binary % 2;
-		binary /= 2;
-		res += rem * pow(2, i);
-		++i;
-		++count;
-		if (count == 4)
-			res = res << count;
-			continue;
+		rem = binary % 10;
+		hexa = hexa + rem * i;
+		i = i * 2;
+		binary /= 10;
 	}
+	return hexa;
+}
+void hexToBin(char *hex)
+{
+	long int i = 0;
+	while (hex[i])
+	{
+		switch (hex[i])
+		{
+		case '0':
+			printf("0000");
+			break;
+		case '1':
+			printf("0001");
+			break;
+		case '2':
+			printf("0010");
+			break;
+		case '3':
+			printf("0011");
+			break;
+		case '4':
+			printf("0100");
+			break;
+		case '5':
+			printf("0101");
+			break;
+		case '6':
+			printf("0110");
+			break;
+		case '7':
+			printf("0111");
+			break;
+		case '8':
+			printf("1000");
+			break;
+		case '9':
+			printf("1001");
+			break;
+		case 'A':
+			printf("1010");
+			break;
+		case 'B':
+			printf("1011");
+			break;
+		case 'C':
+			printf("1100");
+			break;
+		case 'D':
+			printf("1101");
+			break;
+		case 'E':
+			printf("1110");
+			break;
+		case 'F':
+			printf("1111");
+			break;
 
-	dec = dec | res;
-	return dec;
+		}
+		i++;
+		}
+  
 }
--- main1/main1/strtok.c
+++ main1/main1/strtok.c
@@ -1,19 +1,32 @@
 #include <stdio.h>
 #include <string.h>
+char *strtok1(char *str,const char *delim);
 int main()
 {
-     char str[]="This-is-C-Assignment";
+     char str[30];
+     char s[2];
      char *token;
-     char s[2]="-";
-	 int i,j,len;
-	 len = strlen(str);
-	 
-
-   /* token=strtok(str,s);
-   while(token!=NULL)
-   {
-        printf("%s\n",token);
-        token=strtok(NULL,s);
-    }
-    return 0;*/
+     printf("enter the string:\n");
+     gets(str);
+     printf("enter the delimiter character:\n");
+     gets(s);
+     token=strtok1(str,s);
+     while(token!=NULL)
+      {
+           printf("%s\n",token);
+            token=strtok1(NULL,s);
+       }
+     return 0;	
 }
+char *strtok1(char *str,const char *delim)
+{
+     char *token;
+      if(*str!='\0')
+        { while(*str!=*delim)
+            { *token=*str;
+               str++;
+               token++;}
+           }
+}
+     
+        
