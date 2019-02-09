85b793b94396f76e32be25ab9a735f7b3e939e9a
soumyasajja <sajjasoumya@gmail.com>
2019-02-09 18:09:16 +0530

    question 15

---
+++ main1/main1/strtok.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+     char str[]="This-is-C-Assignment";
+     char *token;
+     char s[2]="-";
+	 int i,j,len;
+	 len = strlen(str);
+	 
+
+   /* token=strtok(str,s);
+   while(token!=NULL)
+   {
+        printf("%s\n",token);
+        token=strtok(NULL,s);
+    }
+    return 0;*/
+}
