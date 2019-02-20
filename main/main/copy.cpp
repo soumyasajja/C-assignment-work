2c9d9d11afd4c54ab48add279730016b281d6819
sajjasoumya@gmail.com <sajjasoumya@gmail.com>
2019-02-20 15:20:31 +0530

    infix to prefix

--- main/main/copy.cpp
+++ main/main/copy.cpp
@@ -5,23 +5,37 @@ class Copy
 {
 	char* stored;
 public:
+
 	Copy()
 	{
 		stored = NULL;
+	}
+	Copy(char *s)
+	{
+		stored = s;
 	}
 	Copy(const char *str)
 	{
 		stored = new char[strlen(str) + 1];
 		strcpy(stored, str);
+
+	}
+
+	void display()
+	{
+		cout << stored;
 	}
 	~Copy()
 	{
 		delete[] stored;
 	}
-
 };
 int main()
 {
-	Copy c1,c2(c1);
+	Copy c1, c2("soumya"), c3(c2);
+
+	c2.display();
+	c3.display();
 	return 0;
+
 }
