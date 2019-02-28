a9b513c4a618c289e28d55e4c7aa88d28ff37ef1
sajjasoumya@gmail.com <sajjasoumya@gmail.com>
2019-02-28 06:19:02 +0530

    Processes

--- windows/Process/Process.cpp
+++ windows/Process/Process.cpp
@@ -10,4 +10,40 @@ void _tmain(int argc, TCHAR *argv[], TCHAR *env[])
 	HANDLE hproc;
 	ZeroMemory(&si1, sizeof(si1));
 	ZeroMemory(&si2, sizeof(si2));
+	si1.cb = sizeof(si1);
+	si2.cb = sizeof(si2);
+	ZeroMemory(&pi1, sizeof(pi1));
+	ZeroMemory(&pi2, sizeof(pi2));
+	sa.nLength = sizeof(sa);
+	sa.lpSecurityDescriptor = NULL;
+	sa.bInheritHandle = TRUE;
+	if (argc != 3)
+	{
+		printf("Usage\n");
+		getchar();
+		return;
+	}
+	if (!CreateProcess(NULL, argv[1], NULL, NULL, FALSE, 0, NULL, NULL, &si1, &pi1))
+	{
+		printf("Create Process error (%d)\n", GetLastError());
+		getchar();
+		return;
+	}
+	printf("Handle of the process:%d\n", pi1.hProcess);
+	printf("Process ID of proces:%d\n", pi1.dwProcessId);
+	printf("Process thread of process:%d", pi1.dwThreadId);
+	CloseHandle(pi1.hThread);
+	CloseHandle(pi1.hProcess);
+	if (!CreateProcess(NULL, argv[2], NULL, NULL, FALSE, 0, NULL, NULL, &si2, &pi2))
+	{
+		printf("Create Process error (%d)\n", GetLastError());
+		getchar();
+		return;
+	}
+	printf("Handle of the process:%d\n", pi2.hProcess);
+	printf("Process ID of proces:%d\n", pi2.dwProcessId);
+	printf("Process thread of process:%d", pi2.dwThreadId);
+	CloseHandle(pi2.hThread);
+	CloseHandle(pi2.hProcess);
+	//CloseHandle(hproc);
 }
